6ed277c55c50f54d6f7ab1200fddf33c72f68321
asus-nb\admin <inessa134@gmail.com>
2022-05-19 17:40:41 +0300

    первые два задания проверить

--- task2.js
+++ task2.js
@@ -1,11 +1,10 @@
-let ar =['smth','text','silly']
-function fun (ar, str){
-    let indexOfWord = ar.indexOf(str)
-    if(indexOfWord !==-1){
-      ar.splice(indexOfWord, 1);
-    }
-  return ar
+let ar = ["smth", "text", "silly"];
+function fun(ar, str) {
+  let indexOfWord = ar.indexOf(str);
+  if (indexOfWord !== -1) {
+    ar.splice(indexOfWord, 1);
+  }
+  return ar;
 }
 ////////////
- fun(ar, 'silly')
-
+fun(ar, "silly");
